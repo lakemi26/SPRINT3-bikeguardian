66d749385472843d853f61f0edd66a0611b762d9
lakemi26 <lakemi@outlook.com.br>
2023-09-11 03:45:31 -0300

    images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
-    <h1>hello</h1>
-
+    <>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Link to="/" className='navbar_logo'>
+        Porto
+        </Link>
+      </Switch>
+    </Router>
+    </>
   );
 }
 
