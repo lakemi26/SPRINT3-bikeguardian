79b7fd5a582c6baf73d305dcb44e5107d76cb057
lakemi26 <lakemi@outlook.com.br>
2023-09-11 20:16:19 -0300

    Pag Login

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,6 +1,71 @@
-import React from 'react';
-import '../../App.css';
+import { useState } from "react";
+import { Link } from 'react-router-dom';
+import "./Login.css";
+import FormInput from "../FormInput";
 
-export default function Login() {
-  return <h1 className='login'>Login</h1>;
-}
+const Login = () => {
+  const [values, setValues] = useState({
+    email: "",
+    senha: "",
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "Insira um endereço de email válido!",
+      label: "",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "senha",
+      type: "password",
+      placeholder: "Senha",
+      errorMessage:
+        "Sua senha deve conter entre 8-20 caracteres e incluir pelo menos 1 letra, 1 número e 1 caractere especial!",
+      label: "",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  return (
+  
+    <div className="login">
+      <div className="imagemLogin">
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <p>
+          Não tem uma conta? Crie{" "}
+          <Link 
+          to="/solicite">aqui!</Link>
+        </p>
+        <button>
+          <a href="https://www.exemplo.com">Entrar</a>
+        </button>
+      </form>
+    </div>
+  </div>  
+  );
+};
+
+export default Login;
