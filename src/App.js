632ebbed5f8b760db38050d096feca678695752c
lakemi26 <lakemi@outlook.com.br>
2023-09-12 18:46:07 -0300

    Detalhes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Footer from './components/Footer';
 import CriarConta from './components/pages/CriarConta';
 import AncientCode from './components/pages/AncientCode';
 import PagClie from './components/pages/PagClie';
+import CadastroBike from './components/pages/CadastroBike';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path='/solicite' component={Solicite} />
           <Route path='/AncientCode' component={AncientCode} />
           <Route path='/PagClie' component={PagClie} />
+          <Route path='/CadastroBike' component={CadastroBike}/>
           
         </Route>
         <Footer/>
